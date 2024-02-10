f9aa6ac127bd635143bab23712dee6d57b21e07a
jose alvarez <josemiguelalvarez2014@gmail.com>
2024-02-10 23:14:37 +0000

    add properties in class searchcriteriadto

--- src/shared/interfaces/search-criteria.dto.ts
+++ src/shared/interfaces/search-criteria.dto.ts
@@ -1,6 +1,6 @@
 export class SearchCriteriaDto {
     // [key: string]: number | string | string[] | number[];
-    // sort?: string;
-    // page?: number;
-    // limit?: number;
+    sort?: string;
+    page?: number;
+    limit?: number;
 }
